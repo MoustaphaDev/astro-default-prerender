2281c2d440f96f84fa9dcd90980414049625c3af
Moustapha HappyDev <moustapha.happydev@gmail.com>
2023-03-25 21:40:57 +0000

    hook integration to demo

--- demo/astro.config.ts
+++ demo/astro.config.ts
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
+import astroDefaultPrerender from 'astro-default-prerender';
 
 // https://astro.build/config
-export default defineConfig({});
+import node from '@astrojs/node';
+
+// https://astro.build/config
+export default defineConfig({
+    integrations: [astroDefaultPrerender()],
+    output: 'server',
+    adapter: node({
+        mode: 'standalone',
+    }),
+});
