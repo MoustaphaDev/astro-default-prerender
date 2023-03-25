79458da93bc01042d77f92fe21140ed19a1469a3
Moustapha HappyDev <moustapha.happydev@gmail.com>
2023-03-25 23:41:44 +0000

    use es-module-lexer parse exports

--- packages/integration/src/index.ts
+++ packages/integration/src/index.ts
@@ -3,6 +3,7 @@ import type { Plugin } from 'vite';
 import { parse } from '@astrojs/compiler';
 import { walk, is, serialize } from '@astrojs/compiler/utils';
 import kleur from 'kleur';
+import { type ExportSpecifier, init, parse as parseESM } from 'es-module-lexer';
 
 export default function integration(): AstroIntegration {
     return {
@@ -22,9 +23,7 @@ function getVitePluginInjector(): Plugin {
     return {
         name: 'vite-plugin-astro-default-prerender-injector',
         configResolved(resolved) {
-            if (isHooked) return;
-            (resolved.plugins as Plugin[]).unshift(getVitePlugin());
-            isHooked = true;
+            // TODO: limit this to only run once
         },
     };
 }
@@ -36,24 +35,35 @@ function getVitePlugin(): Plugin {
             if (!id.endsWith('.astro')) return;
             const { ast } = await parse(code);
             let foundFrontmatter = false;
-            walk(ast, (node) => {
+            let didChange = false;
+            await init;
+            walk(ast, async (node) => {
                 if (foundFrontmatter) return;
                 if (is.frontmatter(node)) {
                     foundFrontmatter = true;
-                    // TODO: use es-module-lexer to catch false possitives?
-                    // if (node.value.includes('export const prerender = true;'))
-                    //     return;
-                    node.value += `export const prerender = true;`;
-                    // console.log({ node, ast: JSON.stringify(ast, null, 2) });
+                    // could regexes be enough here?
+                    let exports: readonly ExportSpecifier[] | undefined;
+                    try {
+                        [, exports] = parseESM(node.value);
+                    } catch (e) {
+                        return;
+                    }
+                    // if there's a `prerender` export, return
+                    if (exports.some((e) => e.n === 'prerender')) {
+                        return;
+                    }
+                    node.value = `\nexport const prerender = true;\n${node.value}`;
+                    didChange = true;
                 }
             });
 
-            // TODO: make a PR for an `asyncWalk` function so
+            // TODO: make a PR to add a `asyncWalk` utility function so
             // that we can await it instead of using a timeout
             // to wait for the "walk" call to complete
             await wait(1000);
+            if (!didChange) return;
             const result = serialize(ast);
-            // console.log(result);
+            console.log(result);
             return result;
         },
     };
